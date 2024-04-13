9baa81e6eb0a0c4b4398077bcd76a59cf6380c1f
ArchieTheMagnificent <pannesaproduction@gmail.com>
2024-04-13 23:32:15 +0200

    Zmienilem nazwe klasy na "Main" z wielkiej litery, bo przyczepia sie, że metoda main ma ta sama nazwe, co konstruktor klasy

--- main.java
+++ main.java
@@ -1,1 +1,9 @@
-//dupa
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Hello World!");
+            System.out.println("Zaczynamy");
+    }
+
+
+}
