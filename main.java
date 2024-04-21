300b24e6a2b4f41ce1d4ee9792f005e81f6b8cac
Aleksandra Rajkowska <aleksandra.rajkowska.0@gmail.com>
2024-04-21 22:00:41 +0200

    threadPool vs odpalanie w petli for

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import javax.swing.SwingUtilities;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void ma1in(String[] args) {
         SwingUtilities.invokeLater(() -> {
             //SimpleAnimationWithThreadPool animation = new SimpleAnimationWithThreadPool();
             SimpleAnimationWithThreadPool_Runnable_Thread animation = new SimpleAnimationWithThreadPool_Runnable_Thread();
